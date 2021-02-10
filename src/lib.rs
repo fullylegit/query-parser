5610aa2dd294040fc1ed994c81815be6bcdac02c
user <40304161+fullylegit@users.noreply.github.com>
2021-02-10 20:09:54 +1100

    added a test for fractional boosting

--- src/lib.rs
+++ src/lib.rs
@@ -421,6 +421,15 @@ mod tests {
     }
 
     #[test]
+    fn single_term_fractional_boosted() {
+        run_test(|| {
+            let expected = Ok(("", Query::Term(Term::new("word").set_boost(5.05))));
+            let actual = parse("word^5.05");
+            assert_eq!(expected, actual);
+        })
+    }
+
+    #[test]
     fn single_term_fuzzy() {
         run_test(|| {
             let expected = Ok(("", Query::Term(Term::new("word").set_fuzziness(5))));
